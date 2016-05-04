b1dab3ee4f340f342b7751640fcd9bc1463a3392
tiger4th <tiger4thg@gmail.com>
2016-05-05 00:45:25 +0900

    バッチPHP移行

---
+++ onews.php
@@ -0,0 +1,99 @@
+<?php
+set_time_limit(500);
+mb_language('Japanese');
+
+require_once('./phpQuery.php');
+require_once('./key.php');
+
+//フォルダの定義
+define('OUTPUT_ONEWS', './onews/');
+
+$url = 'http://news.yahoo.co.jp/pickup/rss.xml';
+$links = getLinks($url);
+
+$data_list = [];
+foreach ($links as $link) {
+  $data_list[] = getData($link);
+}
+
+$speaker = 'haruka';
+$format = 'wav';
+
+foreach ($data_list as $key => $data) {
+  $text = $data['title'] . $data['description'];
+  $file = getVoiceText($text, $speaker, $format, $apikey);
+  file_put_contents(OUTPUT_ONEWS . 'voice_' . $key . '.' . $format, $file);
+}
+
+$json = json_encode($data_list, JSON_UNESCAPED_UNICODE);
+file_put_contents(OUTPUT_ONEWS . 'articles.json', $json);
+
+echo 'done';
+
+
+function getVoiceText($text, $speaker, $format, $apikey)
+{
+  $url = 'https://api.apigw.smt.docomo.ne.jp/voiceText/v1/textToSpeech?APIKEY=' . $apikey;
+  
+  $data = [
+    'speaker' => $speaker,
+    'text'    => $text,
+    'format'  => $format,
+  ];
+  $data = http_build_query($data, '', '&');
+  
+  $header = [
+    'Content-Type: application/x-www-form-urlencoded',
+    'Content-Length: ' . strlen($data),
+  ];
+  
+  $context = [
+    'http' => [
+      'method'  => 'POST',
+      'header'  => $header,
+      'content' => $data,
+    ]
+  ];
+  
+  return file_get_contents($url, false, stream_context_create($context));
+}
+
+function getLinks($url)
+{
+  $xml = file_get_contents($url);
+  $feed = simplexml_load_string($xml);
+  $links = [];
+  foreach ($feed->channel->item as $item) {
+    $links[] = (string)$item->link;
+  }
+  return $links;
+}
+
+function getData($link)
+{
+  $pq = phpQuery::newDocumentFile($link);
+  $data['link']        = $link;
+  $data['title']       = $pq['.topicsName']['h1']->text();
+  $description         = $pq['.hbody']->text();
+  $description         = removeBrackets($description, '（', '）');
+  $description         = removeBrackets($description, '(', ')');
+  $data['description'] = $description;
+  $data['image']       = $pq['.headlinePic']['img']->attr('data-src');
+  return $data;
+}
+
+function removeBrackets($target, $bracket_start, $bracket_end)
+{
+  while (strpos($target, $bracket_start) !== false && 
+         strpos($target, $bracket_end)   !== false) {
+    $left = explode($bracket_start, $target);
+    $left = $left[0];
+
+    $right = explode($bracket_end, $target);
+    unset($right[0]);
+    $right = implode($bracket_end, $right);
+
+    $target = $left . $right;
+  }
+  return $target;
+}
